fcd4e4abc27249b744814536be6a9d0c8e6a10d3
pelle Stuhr <pellestuhr@MacBook-Air-von-pelle.fritz.box>
2026-04-25 11:31:31 +0200

    login logout verbessert

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ function login() {
   const pw = document.getElementById("pw").value;
 
   if (pw === "1234") {
+    document.getElementById("loginArea").classList.add("hidden");
     document.getElementById("content").classList.remove("hidden");
+    document.getElementById("subtitle").textContent = "Eingeloggt – dein privater Bereich";
   } else {
     alert("Falsches Passwort");
   }
 }
 
-function showPage(pageId) {
-  alert("Button wurde geklickt: " + pageId);
+function logout() {
+  document.getElementById("content").classList.add("hidden");
+  document.getElementById("loginArea").classList.remove("hidden");
+  document.getElementById("pw").value = "";
+  document.getElementById("subtitle").textContent = "Private Website – nur für dich";
+  showPage("dashboard");
+}
 
+function showPage(pageId) {
   const pages = document.querySelectorAll(".page");
 
   pages.forEach(function(page) {
