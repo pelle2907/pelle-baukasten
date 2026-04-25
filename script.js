90fa5d8ab80a9ada00337845ca077cfac454ab07
pelle Stuhr <pellestuhr@MacBook-Air-von-pelle.fritz.box>
2026-04-25 12:08:04 +0200

    navbar + menu

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function login() {
   if (pw === "1234") {
     document.getElementById("loginArea").classList.add("hidden");
     document.getElementById("content").classList.remove("hidden");
-    document.getElementById("subtitle").textContent = "Eingeloggt – dein privater Bereich";
   } else {
     alert("Falsches Passwort");
   }
@@ -14,16 +13,17 @@ function logout() {
   document.getElementById("content").classList.add("hidden");
   document.getElementById("loginArea").classList.remove("hidden");
   document.getElementById("pw").value = "";
-  document.getElementById("subtitle").textContent = "Private Website – nur für dich";
-  showPage("dashboard");
 }
 
 function showPage(pageId) {
   const pages = document.querySelectorAll(".page");
 
-  pages.forEach(function(page) {
-    page.classList.add("hidden");
-  });
+  pages.forEach(p => p.classList.add("hidden"));
 
   document.getElementById(pageId).classList.remove("hidden");
+}
+
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("hidden");
 }
